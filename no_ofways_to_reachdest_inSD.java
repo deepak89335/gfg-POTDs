23f835f058183582c1d0900a7efd02896eb5642f
DEEPAKKUMAR THAKUR <56125632+deepak89335@users.noreply.github.com>
2022-09-16 12:06:13 +0530

    Create no_ofways_to_reachdest_inSD.java
    You are given an integer n and a 2D integer array roads where roads[i] = [ui, vi, timei] means that there is a road between intersections ui and vi that takes timei minutes to travel. You want to know in how many ways you can travel from intersection 0 to intersection n - 1 in the shortest amount of time.
    
    Return the number of ways you can arrive at your destination in the shortest amount of time. Since the answer may be large, return it modulo 109 + 7.

---
+++ no_ofways_to_reachdest_inSD.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+class GFG {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            List<List<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < m; i++) {
+                List<Integer> temp = new ArrayList<>();
+                for (int j = 0; j < 3; j++) {
+                    int x = sc.nextInt();
+                    temp.add(x);
+                }
+                adj.add(temp);
+            }
+
+            Solution obj = new Solution();
+            System.out.println(obj.countPaths(n, adj));
+        }
+    }
+}
+
+
+class Solution {
+ static class Node implements Comparator<Node>{
+       int v;
+       int time;
+       Node(){}
+       Node(int v,int time){
+           this.v = v;
+           this.time = time;
+       }
+       
+       public int compare(Node n1, Node n2){
+           if(n1.time<n2.time){
+               return -1;
+           }
+           if(n1.time>n2.time){
+               return 1;
+           }
+           return 0;
+       }
+   }
+   
+   static int countPaths(int n, List<List<Integer>> roads) {
+        int[] distance = new int[n];
+        int[] ways = new int[n];
+        Arrays.fill(distance,Integer.MAX_VALUE);
+        
+        // Building Adj list
+        List<List<Node>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(List<Integer> a: roads){
+            
+             int u = a.get(0);
+            int v = a.get(1);
+            int t = a.get(2);
+            adj.get(u).add(new Node(v,t));
+            adj.get(v).add(new Node(u,t));
+        }
+        // Dijkstra's  Algo Starts from here 
+        PriorityQueue<Node> pq = new PriorityQueue<Node>(n,new Node());
+        pq.add(new Node(0,0));
+        distance[0] = 0;
+        ways[0] = 1;
+        while(!pq.isEmpty()){
+            Node node = pq.poll();
+            if(distance[node.v] < node.time)continue;
+            for(Node it:adj.get(node.v)){
+                if(distance[node.v] + it.time < distance[it.v]){
+                    ways[it.v] = ways[node.v];
+                    distance[it.v] = distance[node.v] + it.time;
+                    pq.add(new Node(it.v,distance[it.v]));
+                }
+                else if(distance[node.v] + it.time == distance[it.v]){
+                    ways[it.v] = (ways[it.v] + ways[node.v]);
+                }
+            }
+        }
+        
+        return ways[n-1];
+   }
+    
+}
