a0fc51d1eebff63dbe625962457156e79fe00dbc
DEEPAKKUMAR THAKUR <56125632+deepak89335@users.noreply.github.com>
2022-09-20 13:32:57 +0530

    Create 2x2squares_in_triangle.java
    Given the base (in units) of a right-angled isosceles triangle, find the maximum number of 2X2 squares that can fit in the triangle with given base.

---
+++ 2x2squares_in_triangle.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());//testcases
+        
+        while(t-- > 0)
+        {
+            long n = Long.parseLong(read.readLine());//input n
+            
+            Solution ob= new Solution();
+            System.out.println(ob.numberOfSquares(n));
+        }
+    }
+}
+
+class Solution
+{
+    public static long numberOfSquares(long base)
+    {
+        base=(base/2)-1;
+        return (base*(base+1))/2;
+    }
+}
